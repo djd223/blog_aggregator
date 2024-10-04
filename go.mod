16fc99953feef5821d720fbb536d92112778ef83
djd223 <davissond99@gmail.com>
2024-10-04 09:36:55 -0700

    config with creating and reading initial JSON

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/djd223/blog_aggregator
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/djd223/blog_aggregator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config: %+v\n", cfg)
+
+	err = cfg.SetUser("dylan")
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config again: %+v\n", cfg)
+}
